d01aaf8db9a7db1c50272f087c9dee0d5d93b677
Andreas Nazlidis <andreas221b@gmail.com>
2024-05-20 20:19:44 +0200

    refactor

--- src/gfx/grid.cpp
+++ src/gfx/grid.cpp
@@ -1,5 +1,7 @@
 #include "gfx/grid.h"
 
+#include <cassert>
+
 Grid::Grid(const sf::Vector2u& gridSize, const sf::Vector2u& winSize,
            const sf::Vector2u& texSize)
     : grid_size_(gridSize), texture_size_(texSize) {
@@ -7,10 +9,12 @@ Grid::Grid(const sf::Vector2u& gridSize, const sf::Vector2u& winSize,
   origin_.x = windowCenter.x - 3.f * texSize.x / 2.f;
   origin_.y = windowCenter.y - 3.f * texSize.y / 2.f;
   ArrangeCells();
-  CreateLines();
+  CreateGridLines(2.f);
 }
 
 Cell& Grid::GetCellAt(unsigned int row, unsigned int col) {
+  assert(row >= 0 && row < grid_size_.x);
+  assert(col >= 0 && col < grid_size_.y);
   return cells_[row][col];
 }
 
@@ -39,16 +43,18 @@ void Grid::ArrangeCells() {
   }
 }
 
-void Grid::CreateLines() {
+void Grid::CreateGridLines(float thickness) {
   lines_[0].setPosition(origin_.x, origin_.y + texture_size_.y);
-  lines_[0].setSize(sf::Vector2f(texture_size_.x * cells_[0].size(), 1.f));
+  lines_[0].setSize(
+      sf::Vector2f(texture_size_.x * cells_[0].size(), thickness));
 
   lines_[1].setPosition(origin_.x, origin_.y + 2 * texture_size_.y);
-  lines_[1].setSize(sf::Vector2f(texture_size_.x * cells_[0].size(), 1.f));
+  lines_[1].setSize(
+      sf::Vector2f(texture_size_.x * cells_[0].size(), thickness));
 
   lines_[2].setPosition(origin_.x + texture_size_.x, origin_.y);
-  lines_[2].setSize(sf::Vector2f(1.f, texture_size_.x * cells_.size()));
+  lines_[2].setSize(sf::Vector2f(thickness, texture_size_.x * cells_.size()));
 
   lines_[3].setPosition(origin_.x + 2 * texture_size_.x, origin_.y);
-  lines_[3].setSize(sf::Vector2f(1.f, texture_size_.x * cells_.size()));
+  lines_[3].setSize(sf::Vector2f(thickness, texture_size_.x * cells_.size()));
 }
